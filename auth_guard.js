421d3c05f4402d9f898bbc447579482c1e088cf5
pillippekim <pillippekim87@gmail.com>
2026-05-12 09:36:28 +0900

    Add files via upload

--- auth_guard.js
+++ auth_guard.js
@@ -9,6 +9,7 @@ const PAGE_PERMISSIONS = {
   'packaging.html':   { admin: ['all'], worker: ['포장'] },
   'inspection.html':  { admin: ['all'], worker: ['검사'] },
   'cmb.html':         { admin: ['all'], worker: ['혼련(CMB)'] },
+  'cutting.html':     { admin: ['all'], worker: ['재단'] },
   'admin.html':       { admin: ['all'], worker: [] },
   'master.html':      { admin: ['생산관리팀','경영지원팀'], worker: [] },
   'docs_admin.html':  { admin: ['연구개발팀','품질관리팀','생산관리팀'], worker: [] },
